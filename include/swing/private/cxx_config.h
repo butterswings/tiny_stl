9ccd76b7417cb598a0118e73c8238bc14fe409de
BuTTerSwings <july021230@gmail.com>
2024-05-10 15:13:19 +0800

    feat(cxx_config): add cxx constexpr

--- include/swing/private/cxx_config.h
+++ include/swing/private/cxx_config.h
@@ -1,11 +1,22 @@
 #ifndef SWING_PRIVATE_CXX_CONFIG_H
 #define SWING_PRIVATE_CXX_CONFIG_H
 
+#ifndef SWING_CXX14_CONSTEXPR
+  #if __cplusplus >= 201402L
+    #define SWING_CXX14_CONSTEXPR constexpr
+  #endif
+#endif
+
 #ifndef SWING_CXX17_CONSTEXPR
   #if __cplusplus >= 201703L
     #define SWING_CXX17_CONSTEXPR constexpr
   #endif
 #endif
 
+#ifndef SWING_CXX20_CONSTEXPR
+  #if __cplusplus >= 202002L
+    #define SWING_CXX20_CONSTEXPR constexpr
+  #endif
+#endif
 
 #endif // SWING_PRIVATE_CXX_CONFIG_H
