dde80bec87fdd9a70b62509846805f95e73bdfc6
BuTTerSwings <july021230@gmail.com>
2024-05-11 20:38:24 +0800

    feat: add iterator and vector

---
+++ include/swing/iterator.hpp
@@ -0,0 +1,9 @@
+#ifndef SWING_ITERATOR_HPP
+#define SWING_ITERATOR_HPP
+
+namespace swing
+{
+
+}
+
+#endif // SWING_ITERATOR_HPP
---
+++ include/swing/vector.hpp
@@ -0,0 +1,9 @@
+#ifndef SWING_VECTOR_HPP
+#define SWING_VECTOR_HPP
+
+namespace swing
+{
+
+}
+
+#endif // SWING_VECTOR_HPP
